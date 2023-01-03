2984628aac213230c132ecb4a462edcc80f5f3c8
Anand Ramrakhyani <aramrakhyani@tekion.com>
2023-01-03 13:16:14 +0530

    Added two files

---
+++ HashMapImplementation.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class HashMapImplementation
+{
+
+	public static void main(String[] args)
+	{
+		HashMap<Integer, Integer> hashMap1 = new HashMap<>();
+		hashMap1.put(4,56);
+		hashMap1.put(7,68);
+
+		System.out.println(hashMap1);
+	}
+}

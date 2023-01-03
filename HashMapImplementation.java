d0278f16bcce1decfa1095b70e418a223a97c3f3
Anand <anand.ramrakhyani.work7@gmail.com>
2023-01-03 16:22:30 +0530

    removed all but one k-v pair

--- HashMapImplementation.java
+++ HashMapImplementation.java
@@ -7,14 +7,7 @@ class HashMapImplementation
 	{
 		HashMap<Integer, Integer> hashMap1 = new HashMap<>();
 		hashMap1.put(4,56);
-		hashMap1.put(7,68);
-		hashMap1.put(8,100);
-		hashMap1.put(9,12);
-		hashMap1.put(11,15);
-		hashMap1.put(18,199);
-		hashMap1.put(21,23);
-		hashMap1.put(25,234);
-
+		
 		System.out.println(hashMap1);
 	}
 }

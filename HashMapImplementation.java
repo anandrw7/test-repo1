bb297ff678f8be7f7e644312c33c2a7cf754f490
Anand <anand.ramrakhyani.work7@gmail.com>
2023-01-03 16:01:57 +0530

    one value+

--- HashMapImplementation.java
+++ HashMapImplementation.java
@@ -10,8 +10,8 @@ class HashMapImplementation
 		hashMap1.put(7,68);
 		hashMap1.put(8,100);
 		hashMap1.put(9,12);
-		hashMap1.put(15,14);
 		hashMap1.put(11,15);
+		hashMap1.put(18,199);
 
 		System.out.println(hashMap1);
 	}
